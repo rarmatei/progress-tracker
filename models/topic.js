94fca602b5c2cb2fd9783e3081dd709594946b85
Hervin Efrain Garcia Cardona <hervin_rambito@hotmail.com>
2017-09-09 13:36:05 +0100

    add fields: beforeClass, afterClass, friday, and title.

--- models/topic.js
+++ models/topic.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// I think we still need the following fields: beforeClass, afterClass, friday, and title.
 const schema = new Schema({
     afterClass: Number,
     monday: Number,
     wednesday: Number,
-    thursday: Number
+    thursday: Number,
+    friday: Number,
+    beforeclass: Number
 });
 
 const topic = mongoose.model('topic',schema);
