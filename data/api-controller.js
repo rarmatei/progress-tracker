b8e70638687bd30828493418abe8fa2a2c6c56d0
MrDwina <diwina1993@hotmail.com>
2017-09-09 11:44:58 +0100

    I deleted Schema and mongo modules, I changed collection name to topics

--- data/api-controller.js
+++ data/api-controller.js
@@ -1,73 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const router = express.Router();
-const Schema = mongoose.Schema;
-
-// create progres Schema
-const ProgressSchema = new Schema({
-    supjectName: {
-        type: String,
-        required: [true, 'Name field is required']
-    },
-    beforeClass: {
-        type: Number
-    },
-    afterClass: {
-        type: Number
-    },
-    monday: {
-        type: Number
-    },
-    wednesday: {
-        type: Number
-    },
-    thursday: {
-        type: Number
-    },
-    friday: {
-        type: Number
-    }
-});
-
-// create ninja Schema & model
-const StudentSchema = new Schema({
-    studentName: {
-        type: String,
-        required: [true, 'Name field is required']
-    },
-    progressSchudle: ProgressSchema
-});
-
-const Student = mongoose.model('student', StudentSchema);
 
 
-router.post("/api/student", function (req, res, next) {
-    Student.create(req.body).then(function (student) {
+
+router.post("/api/topics", function (req, res, next) {
+    topics.create(req.body).then(function (topics) {
         res.send({
-            studentName: req.body.studentName,
-            supjectName: req.body.progressSchudle.supjectName,
-            beforeClass: req.body.progressSchudle.beforeClass,
-            afterClass: req.body.progressSchudle.afterClass,
-            monday: req.body.progressSchudle.monday,
-            wednesday: req.body.progressSchudle.wednesday,
-            thursday: req.body.progressSchudle.thursday,
-            friday: req.body.progressSchudle.friday
+            supjectName: req.body.supjectName,
+            beforeClass: req.body.beforeClass,
+            afterClass: req.body.afterClass,
+            monday: req.body.monday,
+            tuesday: req.body.tuesday,
+            wednesday: req.body.wednesday,
+            thursday: req.body.thursday,
+            friday: req.body.friday
         });
     }).catch(next);
 })
 
-router.put("/api/student/:id", function (req, res, next) {
-    Student.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
-        Student.findOne({ _id: req.params.id }).then(function (student) {
+router.put("/api/topics/:id", function (req, res, next) {
+    topics.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
+        topics.findOne({ _id: req.params.id }).then(function (topics) {
             res.send({
-                studentName: req.body.studentName,
-                supjectName: req.body.progressSchudle.supjectName,
-                beforeClass: req.body.progressSchudle.beforeClass,
-                beforeClass: req.body.progressSchudle.beforeClass,
-                monday: req.body.progressSchudle.monday,
-                wednesday: req.body.progressSchudle.wednesday,
-                thursday: req.body.progressSchudle.thursday,
-                friday: req.body.progressSchudle.friday
+                supjectName: req.body.supjectName,
+                beforeClass: req.body.beforeClass,
+                afterClass: req.body.afterClass,
+                monday: req.body.monday,
+                tuesday: req.body.tuesday,
+                wednesday: req.body.wednesday,
+                thursday: req.body.thursday,
+                friday: req.body.friday
             });
         });
     }).catch(next);
