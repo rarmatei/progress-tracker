72df92030646d17661b90b92a4fc42fe691e69a6
MrDwina <diwina1993@hotmail.com>
2017-09-09 14:50:11 +0100

    I created api folder and addTopic and editTopic files

--- data/api-controller.js
+++ data/api-controller.js
@@ -1,38 +1,33 @@
 const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+// create ninja Schema & model
+const StudentSchema = new Schema({
+    supjectName: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    beforeClass: {
+        type: Number
+    },
+    afterClass: {
+        type: Number
+    },
+    monday: {
+        type: Number
+    },
+    wednesday: {
+        type: Number
+    },
+    thursday: {
+        type: Number
+    },
+    friday: {
+        type: Number
+    }
+});
 
+const Topic = mongoose.model('topics', StudentSchema);
 
-router.post("/api/topics", function (req, res, next) {
-    topics.create(req.body).then(function (topics) {
-        res.send({
-            supjectName: req.body.supjectName,
-            beforeClass: req.body.beforeClass,
-            afterClass: req.body.afterClass,
-            monday: req.body.monday,
-            tuesday: req.body.tuesday,
-            wednesday: req.body.wednesday,
-            thursday: req.body.thursday,
-            friday: req.body.friday
-        });
-    }).catch(next);
-})
-
-router.put("/api/topics/:id", function (req, res, next) {
-    topics.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
-        topics.findOne({ _id: req.params.id }).then(function (topics) {
-            res.send({
-                supjectName: req.body.supjectName,
-                beforeClass: req.body.beforeClass,
-                afterClass: req.body.afterClass,
-                monday: req.body.monday,
-                tuesday: req.body.tuesday,
-                wednesday: req.body.wednesday,
-                thursday: req.body.thursday,
-                friday: req.body.friday
-            });
-        });
-    }).catch(next);
-})
-
-module.exports = router;
+module.exports = Topic;
